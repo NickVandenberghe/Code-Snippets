565d7ae1a412407727cb3bebdec11b5ff37014d5
Nick Vandenberghe <Nick.Vandenberghe@outlook.com>
2018-04-30 14:38:01 +0200

    Added groupBy snippet

---
+++ js/groupBy.js
@@ -0,0 +1,12 @@
+const groupBy = (list, keyGetter) => {
+    const map = new Map();
+    list.forEach((item) => {
+        const key = keyGetter(item);
+        if (!map.has(key)) {
+            map.set(key, [item]);
+        } else {
+            map.get(key).push(item);
+        }
+    });
+    return map;
+}
